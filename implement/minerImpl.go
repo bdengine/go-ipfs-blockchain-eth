da033047c11db2457e5b8abcdfa2de94dadbeba3
刘岭峰 <924161495@qq.com>
2021-12-16 09:28:38 +0800

    新增更新节点状态方法 UpdateAddress() Heartbeat()

--- implement/minerImpl.go
+++ implement/minerImpl.go
@@ -1,6 +1,13 @@
 package implement
 
-import "github.com/ipfs/go-ipfs-auth/standard/model"
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ipfs/go-ipfs-auth/auth-source-eth/contract/ipfs"
+	"github.com/ipfs/go-ipfs-auth/standard/model"
+)
 
 func (a *peerImpl) GetChallenge() (string, error) {
 	cli, contr, err := GetSCTokenContract(a.Client.HttpUrl, a.ContractMap[contractToken].ContractAddr)
@@ -14,4 +21,37 @@ func (a *peerImpl) GetChallenge() (string, error) {
 
 func (a *peerImpl) Mining([]model.IpfsMining) error {
 	return nil
+}
+
+func (a *peerImpl) UpdateAddress(addrList []string) error {
+	ctx := context.Background()
+	cli, ipfsContra, err := GetIpfsContract(a.Client.SocketUrl, a.ContractMap[contractIpfs].ContractAddr)
+	if err != nil {
+		return err
+	}
+	defer cli.Close()
+	var f ExecuteIpfsFunc = func(uid string, contract *ipfs.Ipfs, opts *bind.TransactOpts) (*types.Transaction, error) {
+		peer, err := ipfsContra.AddrPeerMap(nil, a.address)
+		if err != nil {
+			return nil, err
+		}
+		if !peer.Valid {
+			return nil, fmt.Errorf("节点不存在")
+		}
+		return ipfsContra.UpdateAddress(opts, uid, addrList)
+	}
+	return a.ExecuteIpfsTransact(ctx, f)
+}
+
+func (a *peerImpl) Heartbeat() error {
+	ctx := context.Background()
+	cli, ipfsContra, err := GetIpfsContract(a.Client.SocketUrl, a.ContractMap[contractIpfs].ContractAddr)
+	if err != nil {
+		return err
+	}
+	defer cli.Close()
+	var f ExecuteIpfsFunc = func(uid string, contract *ipfs.Ipfs, opts *bind.TransactOpts) (*types.Transaction, error) {
+		return ipfsContra.PeerHeartbeat(opts, uid)
+	}
+	return a.ExecuteIpfsTransact(ctx, f)
 }
