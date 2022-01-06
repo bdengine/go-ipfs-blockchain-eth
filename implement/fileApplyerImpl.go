d888f26e3f519a30ce4a8cace500f618f99c0b74
刘岭峰 <924161495@qq.com>
2022-01-06 11:11:22 +0800

    新增查询file方法

--- implement/fileApplyerImpl.go
+++ implement/fileApplyerImpl.go
@@ -1,9 +1,32 @@
 package implement
 
+import (
+	"github.com/ipfs/go-ipfs-auth/standard/model"
+	"math/big"
+)
+
 func (a *peerImpl) ApplyLocal(cid string) error {
 	return nil
 }
 
 func (a *peerImpl) ApplyRemote(cid string) error {
 	return nil
+}
+
+func (a *peerImpl) GetFile(cid string) (*model.IpfsFile, error) {
+	file, err := a.ipfsContract.GetFile(nil, cid)
+	if err != nil {
+		return nil, err
+	}
+	ipfsFile := model.IpfsFile{
+		Cid:         cid,
+		Owner:       file.Owner.String(),
+		Size:        file.Size.Uint64(),
+		ExpireBlock: file.ExpireBlock.Uint64(),
+	}
+	return &ipfsFile, nil
+}
+
+func (a *peerImpl) GetFileList(n int64) ([]string, error) {
+	return a.ipfsContract.GetFileList(nil, big.NewInt(n))
 }
